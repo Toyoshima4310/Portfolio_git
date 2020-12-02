aee2455905b94ca41e7482f5fb5970f7b28e5f8e
Toyoshima Sumito <st474310@icloud.com>
2020-12-02 11:40:37 +0900

    testcommit from htdocs

---
+++ header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="ja">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="./stylesheet.css" >
+    
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class="logo my-3 text-center">My Portfolio Site</div>
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="link col-6 text-center py-3"><a href="#perform">過去の実績</a></div>
+          <div class="link col-6 text-center py-3"><a href="#skills">言語スキル</a></div>
+          <div class="link col-6 text-center py-3"><a href="#profile">プロフィール</a></div>
+          <div class="link col-6 text-center py-3"><a href="#contact">お問い合わせ</a></div>
+        </div>
+      </div>
---
+++ index.php
@@ -0,0 +1,157 @@
+<?php require('header.php') ?>
+
+      
+
+      <div id="perform" class="title text-center"><span>過</span>去の実績</div>
+      <div class="works mt-4">
+        <div class="work mb-5">
+          <div class="d-flex">
+            <div class="left">実績サムネ</div>
+            <div class="ml-2 right">
+                <div class="workTitle my-2">
+                  錬成教室 やいま塾
+                </div>
+                石垣島にある学習塾さんの<br>
+                ホームページ制作を<br>
+                担当させていただきました。
+            </div>
+          </div>
+          <div class="mt-1 py-3 pr-3 text-right detail">
+            詳細を見る　<span>▼</span>
+          </div>
+        </div>
+        <div class="work mb-5">
+          <div class="d-flex">
+            <div class="left">実績サムネ</div>
+            <div class="ml-2 right">
+                <div class="workTitle my-2">
+                  My Portfolio Site
+                </div>
+                自分のポートフォリオを<br>
+                作成しました。<br>
+            </div>
+          </div>
+          <div class="mt-1 py-3 pr-3 text-right detail">
+            詳細を見る　<span>▼</span>
+          </div>
+        </div>
+      </div>
+
+      <div id="skills" class="title text-center"><span>言</span>語スキル</div>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-6 mt-5">
+            <div class="lngImg"><img src="./images/html5Logo.png" class="img-fluid"></div>
+            <div class="lngName my-1">
+              HTML <span>★★★★★</span>
+            </div>
+            <div class="lngText">
+              基本の構造を理解し、<br>
+              静的サイトを作成できます。
+            </div>
+          </div>
+          <div class="col-6 mt-5">
+            <div class="lngImg"><img src="./images/css3Logo.png" class="img-fluid"></div>
+            <div class="lngName my-1">
+              CSS <span>★★★★★</span>
+            </div>
+            <div class="lngText">
+              作りたいデザインに合わせて、<br>
+              サイトを装飾できます。
+            </div>
+          </div>
+          <div class="col-6 mt-5">
+            <div class="lngImg"><img src="./images/BootstrapLogo.png" class="img-fluid"></div>
+            <div class="lngName my-1">
+              Bootstrap <span>★★★★</span>
+            </div>
+            <div class="lngText">
+              レスポンシブ対応のサイトを<br>
+              スムーズに作成できます。
+            </div>
+          </div>
+          <div class="col-6 mt-5">
+            <div class="lngImg"><img src="./images/javascriptLogo.png" class="img-fluid"></div>
+            <div class="lngName my-1">
+              JavaScript <span>★★★</span>
+            </div>
+            <div class="lngText">
+              静的サイトに、<br>
+              簡単な動きをつけられます。
+            </div>
+          </div>
+          <div class="col-6 mt-5">
+            <div class="lngImg"><img src="./images/vueLogo.png" class="img-fluid"></div>
+            <div class="lngName my-1">
+              Vue.js <span>★★★</span>
+            </div>
+            <div class="lngText">
+              JSの動きを、シンプルな<br>
+              コードで実装できます。
+            </div>
+          </div>
+          <div class="col-6 mt-5">
+            <div class="lngImg"><img src="./images/phpLogo.png" class="img-fluid"></div>
+            <div class="lngName my-1">
+              PHP <span>★★★★</span>
+            </div>
+            <div class="lngText">
+              基本の文法を理解し、<br>
+              データベースと接続できます。
+            </div>
+          </div>
+          <div class="col-6 mt-5">
+            <div class="lngImg"><img src="./images/mysqlLogo.png" class="img-fluid"></div>
+            <div class="lngName my-1">
+              MySQL <span>★★★★</span>
+            </div>
+            <div class="lngText">
+              データベース構造を理解し、<br>
+              データの管理ができます。
+            </div>
+          </div>
+          <div class="col-6 mt-5">
+            <div class="lngImg"><img src="./images/wordpressLogo.png" class="img-fluid"></div>
+            <div class="lngName my-1">
+              Wordpress <span>★★★</span>
+            </div>
+            <div class="lngText">
+              既存テーマでのサイト作成、<br>
+              Wordpress化ができます。
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="profile" class="title text-center"><span>プ</span>ロフィール</div>
+      <div class="proText mt-4">
+        豊島純斗(とよしますみと)<br>
+        1998年4月7日生まれ、沖縄県石垣市出身。<br>
+        千葉大学 理学部 数学情報数理学科 数学コース在学中。<br><br>
+
+        プログラミングなどのWeb制作について学ぶことと、<br>
+        家でゆっくり過ごすことが好きです。<br>
+        今は引退しましたが、大学で３年間<br>
+        ダンスサークルに所属していたこともあり、ダンスも好きです。
+      </div>
+
+      
+
+      <div id="contact" class="title text-center"><span>お</span>問い合わせ</div>
+      <div class="mt-4 text-center">
+        Web制作の依頼等、<br>
+        気軽にご連絡ください。<br>
+        翌日以内に返信いたします。
+      </div>
+      <form action="" class="mt-4">
+        <input type="text" placeholder="お名前" class="input">
+        <input type="text" placeholder="メールアドレス" class="input">
+        <textarea name="" class="input" cols="30" rows="10" placeholder="お問い合わせ内容"></textarea>
+        <input type="submit" class="submit" value="送信">
+      </form>
+
+      <div class="footer text-center">
+        ここに著作権のことを書きます。
+      </div>
+        
+<?php require('footer.php') ?>
